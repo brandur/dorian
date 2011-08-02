ff062778bd0cae52f89d1d82338d5648b604974d
Brandur <brandur@mutelight.org>
2011-08-02 15:55:49 -0600

    * tweets are now grouped by year at /twitter * moved twitter stylesheet to tweets.css.sass to better reflect the name of the controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,8 @@ class TweetsController < ApplicationController
   caches_page :index
 
   def index
-    @tweets = Tweet.all
+    @tweet_count = Tweet.count
+    @tweets_by_year = Tweet.all.group_by{|t| t.published_at.year}
     @tweet_stats = Tweet.count_by('published_at', :group_by => 'month')
   end
 end
