764aea8dfd2ef249197e8fa1cc1e49b79fcc5bdf
Brandur <brandur@mutelight.org>
2011-12-24 10:30:01 -0700

    Secret token for production

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dorian::Application.config.secret_token = '13faf2e8c9cbbf48b53be476a95d4f42f0021183ff2880cb4d62faadd80ac15d04fb52a20d799f80121f2f90c0aaff8dfa2a4938ede078ae779d7ee0d22de8b4'
