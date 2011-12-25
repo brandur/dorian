e414f268a3347688aaad642d6e1064f47a818f81
Brandur <brandur@mutelight.org>
2011-12-25 01:54:03 -0700

    Fixing order on relevant charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,17 @@ class HomeController < ApplicationController
 
   def index
     @actions               = GithubAction.ordered.limit(5)
-    @action_count_by_month = GithubAction.ordered.count_by{|a| a.published_at.beginning_of_month}
+    @action_count_by_month = GithubAction.order('published_at ASC')
+      .count_by{|a| a.published_at.beginning_of_month}
     @articles              = Article.ordered.limit(5)
     @book                  = Book.ordered.first
     @book_count_by_year    = Book.ordered.count_by{|b| b.finished_at.beginning_of_year}
     @fact                  = Fact.ordered.first
-    @fact_count_by_day     = Fact.ordered
+    @fact_count_by_day     = Fact.order('created_at ASC')
       .where('created_at > ?', 30.days.ago)
       .count_by{|f| f.created_at.beginning_of_day}
     @tweets                = Tweet.ordered.limit(10)
-    @tweet_count_by_month  = Tweet.ordered.count_by{|t| t.published_at.beginning_of_month}
+    @tweet_count_by_month  = Tweet.order('published_at ASC')
+      .count_by{|t| t.published_at.beginning_of_month}
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 
   def index
     @tweet_count = Tweet.count
-    @tweet_count_by_month = Tweet.ordered.count_by{|t| t.published_at.beginning_of_month}
-    @tweets_by_year = Tweet.ordered.group_by{|t| t.published_at.year}
+    @tweet_count_by_month = Tweet.order('published_at ASC').count_by{|t| t.published_at.beginning_of_month}
+    @tweets_by_year = Tweet.ordered.reverse.group_by{|t| t.published_at.year}
   end
 end
