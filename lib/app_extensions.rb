8909d8422c9cb16abdcc5593334f3eb26c0bcfe4
Brandur <brandur@mutelight.org>
2012-07-27 23:54:17 -0700

    Support multiple modules of the same type

--- lib/app_extensions.rb
+++ lib/app_extensions.rb
@@ -3,9 +3,9 @@ module AppExtensions
 
   def module_instances
     return unless config.modules
-    config.modules.each do |module_sym|
+    config.modules.each do |module_sym, module_config|
       require "modules/#{module_sym}"
-      yield module_sym.to_s.camelize.constantize.new(config.send(module_sym))
+      yield module_sym.to_s.camelize.constantize.new(module_config)
     end
   end
 end
--- lib/configurable_extensions.rb
+++ lib/configurable_extensions.rb
@@ -2,11 +2,9 @@ require 'ostruct'
 
 module ConfigurableExtensions
   def for_module(sym)
-    config.modules = [] unless config.modules
-    config.modules << sym
     module_config = OpenStruct.new
     yield module_config
-    config.send("#{sym}=", module_config)
+    config.modules ||= []
+    config.modules << [sym, module_config]
   end
 end
-
