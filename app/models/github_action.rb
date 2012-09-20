a9353237d278e7ccfce5ca4b8a1df4a6ce3ca56d
Brandur <brandur@mutelight.org>
2012-09-19 22:33:46 -0700

    Kill gravatars

--- app/models/github_action.rb
+++ app/models/github_action.rb
@@ -5,6 +5,10 @@ class GithubAction < ActiveRecord::Base
 
   def content_html
     c = content.gsub /href\s*=\s*["']([^(http)].*?)["']/, 'href="https://github.com\1"'
+
+    # kill the gravatar images
+    c = content.gsub /<a.*?class="gravatar".*?<\/a>/, ''
+
     c
   end
 end
