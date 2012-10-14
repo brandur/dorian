d17e670c416f7829c2dc92691a2a3375c859671a
Brandur <brandur@mutelight.org>
2012-10-14 08:42:31 -0700

    Not sure why we're removing colons -- take this out

--- lib/modules/twitter.rb
+++ lib/modules/twitter.rb
@@ -8,7 +8,7 @@ class Twitter < ModuleBase
     num_updates = 0
     json_for("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{config.user}") do |json|
       json.each do |item|
-        tweet = Tweet.new :content => item["text"].gsub(/^[^:]+: /, ''), 
+        tweet = Tweet.new :content => item["text"],
           :permalink => "http://twitter.com/#{config.user}/statuses/#{item["id"]}",
           :published_at => Time.parse(item["created_at"])
         if tweet.valid?
